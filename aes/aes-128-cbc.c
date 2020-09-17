c797505ef9fe5413d15fed0729d89babea73fdc6
Jakob <e11809618@student.tuwien.ac.at>
2020-09-17 08:47:53 +0200

    update and new file

--- aes/aes-128-cbc.c
+++ aes/aes-128-cbc.c
@@ -5,20 +5,21 @@
  */
 #include <unistd.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "common.h"
 
 #define MODE "cbc"
-#define KEY_LEN (128)
 
 int main(int argc, char **argv){
     char *direction = "-e";
     char *iv    = "000102030405060708090a0b0c0d0e0f";
     char *key   = "2B7E151628AED2A6ABF7158809CF4F3C";
+    int len = 128;
 
     uint8_t dir = 1;
     int c;
 
-    while( (c = getopt(argc, argv, "dek:i:")) != -1 ){
+    while( (c = getopt(argc, argv, "dek:i:l:")) != -1 ){
 		switch( c ){
 			case 'd':
                 dir = 0;
@@ -34,6 +35,12 @@ int main(int argc, char **argv){
                 iv = optarg;
                 break;
             */
+
+            case 'l':
+                len = strtol(optarg, NULL, 10);
+                if (len != 128 || len != 192 || len != 256)
+                    error_exit("Only 128, 192 or 256 bit keys are allowed");
+                break;
             default:
                 break;
         }
@@ -47,7 +54,7 @@ int main(int argc, char **argv){
         close(pipefd[dir]);
         dup2(pipefd[!dir], dir == 1 ? STDIN_FILENO : STDOUT_FILENO);
         close(pipefd[!dir]);
-        execlp("./aes", "./aes", direction, "-k", key, "-i", iv, "-l", KEY_LEN, "-m", MODE, NULL);
+        execlp("./aes", "./aes", direction, "-k", key, "-i", iv, "-l", len, "-m", MODE, NULL);
 
     } else { 
         close(pipefd[!dir]);
--- aes/test.sh
+++ aes/test.sh
@@ -2,7 +2,7 @@
 # IV and KEY files are ascii hex representation, plain and cipher are binary
 
 cat data/plain | ./pkcs7 -e | ./aes -e \
--k $(cat data/key_aes_128_cbc_001) -i $(cat data/iv_aes_128_cbc_001) -m cbc > data/out
+-k $(cat data/key_aes_128_cbc_001) -i $(cat data/iv_aes_128_cbc_001) -m cbc -l 128 > data/out
 cat data/out |./aes -d \
 -k $(cat data/key_aes_128_cbc_001) -i $(cat data/iv_aes_128_cbc_001) -m cbc | ./pkcs7 -d > data/tmp
 diff data/tmp data/plain && echo success
